2bc6efa5d1cdb2b45d17873e071c0dd7a325f082
BsIsraU08 <111823376+BsIsraU08@users.noreply.github.com>
2022-11-21 14:07:34 -0600

    Script Cypher

---
+++ Encoding & Decoding/Cypher.py
@@ -0,0 +1,31 @@
+#Jesús Israel Bolaños
+#2005587
+
+# Importamos fernet desde cryptography
+#
+from cryptography.fernet import Fernet
+#
+# Definición del funcion genwrite que genera una llave para cifrado
+def genwrite():
+	key = Fernet.generate_key()
+	with open("pass.key","wb") as key_file:
+		key_file.write(key)
+# Llamamos a la funcion para generar el archivo "pass.key que contiene la llave"
+genwrite()
+# Definición de la función call_key con la cual leemos el contenido del archivo "pass.key"
+def call_key():
+	return open("pass.key","rb").read()
+# Ahora cifraremos un mensaje almacenado y codificado previamente
+key = call_key()
+banner = "Ya entró la temporada de otoño".encode()
+a = Fernet(key)
+coded_banner = a.encrypt(banner)
+print(coded_banner)
+#
+# Ahora desciframos el mensaje previamente cifrado
+key = call_key()
+b = Fernet(key)
+decoded_banner = b.decrypt(coded_banner)
+print(decoded_banner)
+#
+# Fin del script
